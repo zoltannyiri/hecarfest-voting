b4adad57d340d3ebdf547853fcd4f4bc3bd7b2b5
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2024-09-27 17:02:27 +0200

    page

--- src/app/kezdolap/kezdolap.component.ts
+++ src/app/kezdolap/kezdolap.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-kezdolap',
   standalone: true,
-  imports: [KezdolapComponent],
+  imports: [KezdolapComponent, FormsModule, CommonModule],
   templateUrl: './kezdolap.component.html',
   styleUrl: './kezdolap.component.scss'
 })
 export class KezdolapComponent {
+  options = [
+    { title: 'Szépvégverseny', isOpen: true, plate: '' },
+    { title: 'Opció 2', isOpen: true, plate: '' },
+    { title: 'Opció 3', isOpen: true, plate: '' }
+  ];
 
+  toggleOption(index: number) {
+    this.options[index].isOpen = !this.options[index].isOpen;
+  }
+
+  submit(index: number) {
+    alert(`A beküldött rendszám az ${this.options[index].title} számára: ${this.options[index].plate}`);
+    this.options[index].isOpen = false; // Becsukja az opciót a küldés után
+  }
 }
